d575023e57cf212fd7edc54928ffbe943d746bcd
greatPasta <coolmail2011@yandex.ru>
2017-10-01 16:42:25 +0500

    данные для задачи

---
+++ database/migrations/2017_10_01_102508_create_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->integer('brand_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categorys')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,71 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$catQuantity = 2;
+		$brandQuantity = 2;
+		$propQuantity = 1;
+		$propValQuantity = 2;
+
+		for($i = 1; $i <= $brandQuantity; $i++) {
+			DB::table('brands')->insert([
+				'id' => $i,
+				'name' => 'Брэнд № ' . $i,
+			]);
+		}
+
+		for($i = 1; $i <= $catQuantity; $i++) {
+			DB::table('categorys')->insert([
+				'id' => $i,
+				'name' => 'Категория № ' . $i,
+			]);
+		}
+
+		for($i = 1; $i <= $propQuantity; $i++) {
+			DB::table('propertys')->insert([
+				'id' => $i,
+				'name' => 'Свойство № ' . $i,
+			]);
+		}
+
+		$i = 0;
+		for($bi = 1; $bi <= $brandQuantity; $bi++) {
+			for($ci = 1; $ci <= $catQuantity; $ci++) {
+				$i++;
+				DB::table('products')->insert([
+					'id' => $i,
+					'name' => 'Продукт № ' . $i,
+					'category_id' => $ci,
+					'brand_id' => $bi,
+				]);
+			}
+		}
+		$productQuantity = $i;
+
+		$i = 0;
+		for($pi = 1; $pi <= $propQuantity; $pi++) {
+			for($pvi = 1; $pvi <= $propValQuantity; $pvi++) {
+				$i++;
+				DB::table('property_values')->insert([
+					'id' => $i,
+					'name' => 'Значение № ' . $i,
+					'property_id' => $pi,
+				]);
+			}
+		}
+
+		for($i = 1; $i <= $productQuantity; $i++) {
+			DB::table('products_property_values')->insert([
+				'id' => $i,
+				'product_id' => $i,
+				'property_value_id' => mt_rand(1, $propValQuantity)
+			]);
+		}
+	}
 }
